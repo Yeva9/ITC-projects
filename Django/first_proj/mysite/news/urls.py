88965faaab29f9a8ea68ed6994dcc719efa523f2
yeva <eva.hovnanyan.1999@gmail.com>
2021-07-11 19:07:10 +0430

    Added super user.

--- Django/first_proj/mysite/news/urls.py
+++ Django/first_proj/mysite/news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, test
+from .views import index
+# from .views import test
 
 urlpatterns = [
     path('', index),
-    path('test/', test)
+    # path('test/', test)
 ]
--- Django/first_proj/mysite/news/views.py
+++ Django/first_proj/mysite/news/views.py
@@ -1,16 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import News
 
 
 def index(request):
     # print(dir(request))
-    news = News.objects.all()
-    res = '<h1>NORUTYUNNER</h1>'
-    for it in news:
-        res += f'<div><p>{it.title}</p><p>{it.content}</p></div><hr>'
-    return HttpResponse(res)
+    # res = '<h1>NORUTYUNNER</h1>'
+    # for it in news:
+    #     res += f'<div><p>{it.title}</p><p>{it.content}</p></div><hr>'
+    # return HttpResponse(res)
 
+    news = News.objects.order_by("-created_at")
 
-def test(request):
-    return HttpResponse('<h1>TESTTTT</h1>')
+    context = {
+        'news': news,
+        'title': 'Norutyunneri cank'
+    }
+
+    return render(request, 'news/index.html', context)
+
+#
+# def test(request):
+#     return HttpResponse('<h1>TESTTTT</h1>')
