f2f3753334392930b3e405c53b332f38897f787c
yeva <eva.hovnanyan.1999@gmail.com>
2021-07-18 16:51:56 +0430

    Used the generated custom template tag

--- Django/first_proj/mysite/news/views.py
+++ Django/first_proj/mysite/news/views.py
@@ -4,19 +4,10 @@ from .models import News, Category
 
 
 def index(request):
-    # print(dir(request))
-    # res = '<h1>NORUTYUNNER</h1>'
-    # for it in news:
-    #     res += f'<div><p>{it.title}</p><p>{it.content}</p></div><hr>'
-    # return HttpResponse(res)
-
-    # news = News.objects.order_by("-created_at")
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Norutyunneri cank',
-        'categories': categories
+        'title': 'Norutyunneri cank'
     }
 
     return render(request, template_name='news/index.html', context=context)
@@ -24,11 +15,9 @@ def index(request):
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category
     }
     return render(request, template_name='news/category.html', context=context)
