3dede7e9dd1c83ea536b37686709869c411ae5d6
yeva <eva.hovnanyan.1999@gmail.com>
2021-07-25 21:47:22 +0430

    Added get_object_or_404 function.

--- Django/first_proj/mysite/news/views.py
+++ Django/first_proj/mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
 
@@ -24,5 +24,6 @@ def get_category(request, category_id):
 
 
 def view_news(request, news_id):
-    news_item = News.objects.get(pk=news_id)
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {"news_item": news_item})
