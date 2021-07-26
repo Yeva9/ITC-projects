0e4e805d7929e43b8197d6cab12ad821dba2c947
yeva <eva.hovnanyan.1999@gmail.com>
2021-07-27 01:54:53 +0430

    Added forms.py and 'add_news.html' for News.

---
+++ Django/first_proj/mysite/news/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Category
+
+
+class NewsForm(forms.Form):
+    title = forms.CharField(max_length=150, label='Anun ', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }))
+    content = forms.CharField(label='Text ', required=False, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'rows' : 5
+        }))
+    is_published = forms.BooleanField(label='Hraparakvel e? ', initial=True)
+    category = forms.ModelChoiceField(empty_label='Yntreq Category',
+        label='Category ', queryset=Category.objects.all(), widget=forms.Select(
+        attrs={
+            'class': 'form-control'
+        }))
