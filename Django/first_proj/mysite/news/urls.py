5b22e120fd4d8bc5b27fc4a258e01c8ed956b5d8
yeva <eva.hovnanyan.1999@gmail.com>
2021-08-08 11:50:18 -0400

    Added class DetailView.

--- Django/first_proj/mysite/news/urls.py
+++ Django/first_proj/mysite/news/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
 # from .views import index, get_category
-from .views import view_news, add_news, HomeNews, NewsByCategory
+from .views import view_news, add_news, HomeNews, NewsByCategory, ViewNews
 
 urlpatterns = [
     # path('', index, name='home'),
-    path('', HomeNews.as_view(), name='home'),
+
     # path('category/<int:category_id>/', get_category, name='category'),
     # path('category/<int:category_id>/', NewsByCategory.as_view(extra_context=
     #                                     {'title': 'CATEGORIES'}), name='category'),
+    # path('news/<int:news_id>/', view_news, name='view_news'),
+    path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
-    path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
     path('news/add_news/', add_news, name='add_news'),
 ]
