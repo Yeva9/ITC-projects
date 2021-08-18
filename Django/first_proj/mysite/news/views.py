f92da943e06dec6362a94f149ec0d26d984e6ac5
yeva <eva.hovnanyan.1999@gmail.com>
2021-08-18 19:15:31 -0400

    Fixed UI.

--- Django/first_proj/mysite/news/views.py
+++ Django/first_proj/mysite/news/views.py
@@ -40,6 +40,7 @@ class NewsByCategory(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
